7f541930e09462c70a19235d20f021bd75dbf39b
iELVEE <ommarashid@gmail.com>
2023-09-01 14:03:04 -0400

    changed invoke-expression of profile to direct program call for better security

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -15,11 +15,32 @@ Set-Alias -Name cls -Value Clear-Host
 
 #list all current versions for languages and programs installed
 # if there is a program to add, add it to $progs
-function versions
-{
-	$progs = "javac", "java", "python", "git", "gcc" , "g++", "gdb"
-	foreach ($progname in $progs){
-		Invoke-Expression ($progname + " --version")
+function versions{
+	$progs =	"C:\Program Files\Microsoft\jdk-11.0.16.101-hotspot\bin\javac.exe",
+				"C:\Program Files\Microsoft\jdk-11.0.16.101-hotspot\bin\java.exe",
+				"C:\Users\ommar\AppData\Local\Programs\Python\Python311\python.exe",
+				"C:\Program Files\Git\cmd\git.exe",
+				"C:\msys64\mingw64\bin\gcc.exe",
+				"C:\msys64\mingw64\bin\g++.exe",
+				"C:\msys64\mingw64\bin\gdb.exe"
+	foreach ($prog_name in $progs){
+		& $prog_name --version
 		echo ""
 	}
+
 }
+
+<#
+function mwdgr([string]$repo){
+	Invoke-Expression("git init")
+	Invoke-Expression("git remote add origin " + $repo)
+
+	$files = Get-ChildItem
+	foreach ($file_name in $files){
+		Invoke-Expression("git add " + $file_name)
+	}
+
+	Invoke-Expression("git commit -am \"initial commit\"")
+	Invoke-Expression("git push -u -f origin main")
+}
+#>
