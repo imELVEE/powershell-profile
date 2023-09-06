93e73af26d424ef3960308c2f9c9bb731d202d28
iELVEE <ommarashid@gmail.com>
2023-09-06 17:03:53 -0400

    remove -f flag so as to not accidentally remove or overwrite unintended files

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -46,5 +46,15 @@ function mwdgr([string]$repo){
 	& $git_prog add -A
 	& $git_prog commit -am "inital commits"
 	& $git_prog pull --rebase origin main
-	& $git_prog push -u -f origin main
+	& $git_prog push -u origin main
+}
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
 }
