6bdf3271f43b25f257d3381d9da2172e89f83367
iELVEE <ommarashid@gmail.com>
2023-09-09 16:12:16 -0400

    a little formatting and added a preamble adder for creating files: authoredTemplate

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,50 +11,72 @@ oh-my-posh init pwsh --config 'C:\Users\ommar\AppData\Local\Programs\oh-my-posh\
 Set-Alias -Name cls -Value Clear-Host
 
 
-#######useful functions
+## Useful functions
 
-<# list all current versions for languages and programs installed
-	#if there is a program to add, add it to $progs
-## @pre these programs exist in their current locations
-#>
-function versions{
-	$progs =	"C:\Program Files\Microsoft\jdk-11.0.16.101-hotspot\bin\javac.exe",
-				"C:\Program Files\Microsoft\jdk-11.0.16.101-hotspot\bin\java.exe",
-				"C:\Users\ommar\AppData\Local\Programs\Python\Python311\python.exe",
-				"C:\Program Files\Git\cmd\git.exe",
-				"C:\msys64\mingw64\bin\gcc.exe",
-				"C:\msys64\mingw64\bin\g++.exe",
-				"C:\msys64\mingw64\bin\gdb.exe"
-	foreach ($prog_name in $progs){
-		& $prog_name --version
-		echo ""
+	<# list all current versions for languages and programs installed
+		#if there is a program to add, add it to $progs
+	## @pre		: these programs exist in their current locations
+	#>
+	function versions
+	{
+		$progs =	"C:\Program Files\Microsoft\jdk-11.0.16.101-hotspot\bin\javac.exe",
+					"C:\Program Files\Microsoft\jdk-11.0.16.101-hotspot\bin\java.exe",
+					"C:\Users\ommar\AppData\Local\Programs\Python\Python311\python.exe",
+					"C:\Program Files\Git\cmd\git.exe",
+					"C:\msys64\mingw64\bin\gcc.exe",
+					"C:\msys64\mingw64\bin\g++.exe",
+					"C:\msys64\mingw64\bin\gdb.exe"
+		foreach ($prog_name in $progs){
+			& $prog_name --version
+			echo ""
+		}
+
 	}
 
-}
+	<# turn current working directory into remote git repo and sync/link with online version
+		#if git init is defaulting to 'master' in your local repository, run <git config --global init.defaultBranch main> in your cli
+			#this changes the default name git assigns to your local branch from master to main
+	## @pre		: an existing repo on github.com to push to
+	## @param	: string, the SSH clone link of online repo
+	## @post	: local repo is created and linked to an existing online repo
+	#>
+	function gitRepo([string]$repo)
+	{
+		$git_prog = "C:\Program Files\Git\cmd\git.exe"
+		& $git_prog init
+		& $git_prog remote add origin $repo
+		& $git_prog add -A
+		& $git_prog commit -am "inital commits"
+		& $git_prog pull --rebase origin main
+		& $git_prog push -u origin main
+	}
 
-<# turn current working directory into remote git repo and sync/link with online version
-	#if git init is defaulting to 'master' in your local repository, run <git config --global init.defaultBranch main> in your cli
-		#this changes the default name git assigns to your local branch from master to main
-## @pre1 an existing repo on github.com to push to
-## @post local repo is created and linked to an existing online repo
-## @param $repo is the SSH clone link of online repo
-#>
-function mwdgr([string]$repo){
-	$git_prog = "C:\Program Files\Git\cmd\git.exe"
-	& $git_prog init
-	& $git_prog remote add origin $repo
-	& $git_prog add -A
-	& $git_prog commit -am "inital commits"
-	& $git_prog pull --rebase origin main
-	& $git_prog push -u origin main
-}
+	<# take coding preamble and create a file with given name
+	## @pre		: template txt file exists properly formatted with <date> and <file_name> to replace
+					no conditionals/safety checks because this is for personal use and I'm feeling particularly lazy today
+	## @param	: string, the name of the file to be created with the template
+	## @param	: string, path to a "template.txt" file containing *only* the preamble,
+					default value is a path to ~\Documents
+	## @post	: create a file with given name in the working directory
+	#>
+	function authoredTemplate([string]$file_name, [string]$templateFile = "C:\Users\ommar\Documents\")
+	{
+		$templateFile += "template.txt"
+		[string] $templateText = Get-Content -Path $templateFile -Raw
+		[string] $date = Get-Date -Format "MM/dd/yyyy"
+		$templateText = $templateText.replace("<date>", $date).replace("<file_name>", $file_name)
+		Set-Content -Path $file_name -Value $templateText
+	}
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
+
+## Automatically added code during installations
+
+	# Import the Chocolatey Profile that contains the necessary code to enable
+	# tab-completions to function for `choco`.
+	# Be aware that if you are missing these lines from your profile, tab completion
+	# for `choco` will not function.
+	# See https://ch0.co/tab-completion for details.
+	$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+	if (Test-Path($ChocolateyProfile)) {
+	  Import-Module "$ChocolateyProfile"
+	}
